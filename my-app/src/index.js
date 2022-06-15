81c378363260ff2d8c06091bf6afb58580fb749b
lien-jiseo <jiin@lien.kr>
2022-06-15 15:17:02 +0900

    Square 컴포넌트에 state 추가하고 초기화
    - Square 컴포넌트에 constructor 추가
    - state 초기화

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,9 +3,18 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 
 class Square extends React.Component {
+  // 하위 클래스의 생성자(constructor)를 정의 할 떄 super를 호출해야함.
+  // 모든 React의 class conponent는 생성자를 가질 때 super(props) 호출 구문을 작성해야함 .
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: null,
+    };
+  }
+
   render() {
     return (
-      // onClick={console.log('click')} 형식으로 작성할 경우 경고창이 뜰 수 있기 때문에 화살표 함수를 사용한 아래의 형식으로 작성
+      // onClick={console.log('click')} 형식으로 작성할 경우 경고창이 뜰 수 있기 때문에 화살표 함수를 사용한 아래의 형식으로 작성.
       <button className="square" onClick={() => console.log("click")}>
         {this.props.value}
       </button>
@@ -15,7 +24,7 @@ class Square extends React.Component {
 
 class Board extends React.Component {
   renderSquare(i) {
-    return <Square value={i}/>;
+    return <Square value={i} />;
   }
 
   render() {
