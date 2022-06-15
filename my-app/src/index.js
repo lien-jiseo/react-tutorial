e3873d8b4aa2ee8500833495b746bcae3977e982
lien-jiseo <jiin@lien.kr>
2022-06-15 16:37:31 +0900

    Board 컴포넌트에 renderSquare함수 추가
    - onClick을 추가해 Square 클릭 시 함수 호출

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -23,15 +23,20 @@ class Square extends React.Component {
 }
 
 class Board extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.state={
-      squares:Array(9).fill(null),
+    this.state = {
+      squares: Array(9).fill(null),
     };
   }
 
   renderSquare(i) {
-    return <Square value={this.state.squares[i]} />;
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
+    );
   }
 
   render() {
